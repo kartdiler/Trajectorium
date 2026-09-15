d7ec9ce72c1021c65abac11f777d65034501d002
kartdiler <riderk12t@gmail.com>
2026-09-16 00:20:06 +0300

    Added black space

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
-void main() {
-    
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+    static void main() {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Canvas canvas = new Canvas(1000, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        root.getChildren().add(canvas);
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Trajectorium");
+        stage.setWidth(1000);
+        stage.setHeight(600);
+        stage.show();
+    }
 }
